3703a730bb1a9ccaa1fe6adc87716141aea320dc
florian.work@gmail.com <florian.work@gmail.com>
2015-03-20 18:09:03 +0100

    rename pris en compte

--- read_python3.py
+++ read_python3.py
@@ -18,29 +18,31 @@ path = 'D:\data\BSPP'
 
 tab = read(path)
 tab.columns
+tab.index
 # Quels sont les motifs avec lesquels on se trompe le plus ?
+# Objectif : calculer la durée d'intervention moyenne par motif moyenne
 
-tab[u'Id_Intervention_Abrege_Motif'].value_counts()
-z = tab[u'Id_Intervention_Abrege_Motif'] == tab[u'Code_Cri']
+tab[u'motif_ini'].value_counts()
+z = tab[u'motif_ini'] == tab[u'motif']
 Counter(z)
 
 tab = pd.DataFrame(tab)
 tab.dtypes
 
 #On convertit toutes les variables en "object"
-tab.Code_Cri = tab.Code_Cri.astype(object)
-tab.Id_Intervention = tab.Id_Intervention.astype(object)
+tab.motif = tab.motif.astype(object)
+tab.id_intervention = tab.id_intervention.astype(object)
 
 # On enlève les ".0"
-tab["Code_Cri"] = tab["Code_Cri"].apply(zeros_rm)
-tab["Id_Intervention_Abrege_Motif"] = tab["Id_Intervention_Abrege_Motif"].apply(zeros_rm)
+tab["motif"] = tab["motif"].apply(zeros_rm)
+tab["motif_ini"] = tab["motif_ini"].apply(zeros_rm) #value error
 
 # Les index pour lesquels il y a une erreur
-erreurs = tab.Id_Intervention_Abrege_Motif != tab.Code_Cri
+erreurs = tab.motif_ini != tab.motif
 tab['erreur'] = erreurs
-tab.groupby("Id_Intervention_Abrege_Motif")['erreur'].sum()
+tab.groupby("motif")['erreur'].sum()
 
-tab.groupby(['Id_Intervention_Abrege_Motif', 'Code_Cri']).size()
+tab.groupby(['motif', 'motif']).size()
 
 
 # Objectif : 
