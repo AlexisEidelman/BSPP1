9b6cb244807749bc72707187db16369ac8daa324
florian.work@gmail.com <florian.work@gmail.com>
2015-03-20 17:29:08 +0100

    tools bspp

---
+++ tools_bspp.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 20 17:08:30 2015
+
+@author: Florian
+"""
+
+# Enlever les ".0" à la fin des objets.
+
+def zeros_rm(i):
+    return "%g" % float(i)
