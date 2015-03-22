9daad1e4af41b04cd91f3627043f7f25a38a3e59
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2015-03-22 22:29:24 +0100

    meteo

---
+++ meteo.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 22 19:40:07 2015
+
+@author: alexis
+"""
+
+import pandas as pd
+import os
+
+path = 'D:\data\BSPP\meteo.txt'
+
+tab = pd.read_csv(path)
+del tab['FullMetar']
+del tab['HeureCET']
+
+tab.dropna(how='all', inplace=True)
+header = tab.iloc[0,:]
+
+tab.set_index(pd.DatetimeIndex(tab['DateUTC']), inplace=True)
+
+meteo = tab
+
+
+## étude 
+
+from read import read
+tab = read()
+tab.resample('30min')
+
+# TODO: regarder si quand on a pluie, ou brouillard dans événements,
+# la structure des motifs changent
